679eccaacd26b7279066aaf018fe5e9c071588fc
nebado <ruslan.nbd@gmail.com>
2020-07-16 09:22:18 +0300

    Deleted .emacs.d

--- emacs/.emacs.d/lisp/elpa/cmake-mode-20190710.1319/cmake-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "cmake-mode" "20190710.1319" "major-mode for editing CMake sources" '((emacs "24.1")) :commit "420a75fc3f612bbc938db9e16f8eb22b0118301c")
